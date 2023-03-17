9aa3a9d9613687f9c7ef6beaf26f8831d3f9ca8d
Muhammad_Danial_Aiman BiN_Mohd_Shaharuddin <neutraljs@gmail.com>
2023-03-17 11:22:27 +0800

    End Of S9

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
+import { enviroment } from 'src/environments/enviroment';
 import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'http://localhost:5066/api/';
+  baseUrl = enviroment.apiUrl;
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
